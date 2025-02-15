b601df58aaabe0f2a364dd7d912205d0c55d1dbe
Saritha DR <sarithavinodtvm@gmail.com>
2025-02-16 03:43:17 +0530

    Modal Example - Showing modal as delete book confirmation

--- Project_final/resources/views/viewAllBooks.blade.php
+++ Project_final/resources/views/viewAllBooks.blade.php
@@ -23,9 +23,32 @@
             <td>{{$book->bookDescription}}</td>
             <td>
                 <a href="loadEditBookPage" class="btn btn-success"> Edit </a>
-                <a href="deleteBook" class="btn btn-danger"> Delete </a>
+                <a class="btn btn-danger" onClick="$('#demoModal').modal('show')"> Delete </a>
             </td>
         </tr>
     @endforeach
 </table>
+
+{{--Modal for delete confirmation--}}
+<div class="modal fade" id="demoModal" tabindex="-1" role="dialog" aria-labelledby="demoModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="demoModalLabel">Confirmation - Delete Book</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+			</div>
+			<div class="modal-body">
+				Are you sure you want to delete this book?
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+				<button type="button" class="btn btn-primary">Yes</button>
+			</div>
+		</div>
+	</div>
+</div>
+{{--end of Modal for delete confirmation--}}
+
 @stop
