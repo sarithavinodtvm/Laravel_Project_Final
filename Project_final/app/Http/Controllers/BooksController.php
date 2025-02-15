0b080ca463258a7adc0780e535b202b557546231
Saritha DR <sarithavinodtvm@gmail.com>
2025-02-16 03:04:11 +0530

    Add New Book - PHP Server side validation

--- Project_final/app/Http/Controllers/BooksController.php
+++ Project_final/app/Http/Controllers/BooksController.php
@@ -23,12 +23,18 @@ class BooksController extends Controller
             $title=$request->input("txtName");
             $author=$request->input("txtAuth");
             $description=$request->input("txtDescr");
-            $book=new Book;
-            $book->bookTitle=$title;
-            $book->bookAuthor=$author;
-            $book->bookDescription=$description;
-            $book->save();
-            //echo "inserted successfully";
-            return redirect("viewAllBooks");
+            if($title=="")
+                echo "Title cannot be null";
+            else if($author=="")
+                echo "Author cannot be null";
+            else{
+                $book=new Book;
+                $book->bookTitle=$title;
+                $book->bookAuthor=$author;
+                $book->bookDescription=$description;
+                $book->save();
+                //echo "inserted successfully";
+                return redirect("viewAllBooks");
+            }
     }
 }
