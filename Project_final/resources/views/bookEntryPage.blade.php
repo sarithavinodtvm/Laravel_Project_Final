ebf650b01e1cdab7de1a27d63fc691e6da23f449
Saritha DR <sarithavinodtvm@gmail.com>
2025-02-16 03:02:01 +0530

    Add New Book - Alligning Form elements

--- Project_final/resources/views/bookEntryPage.blade.php
+++ Project_final/resources/views/bookEntryPage.blade.php
@@ -3,21 +3,25 @@
 @section('title', 'Home Page')
 
 @section('content_header')
-    <h1>List of Books</h1>
+    <h1>Add new Book</h1>
 @stop
 
 @section('content')
-<html>
-    <body>
-        <h1>Add New Book</h1>
         <form name="frmReg" method="post" action="saveBookDetails" >
             @csrf
-            Name <input type="text" name="txtName" id="txtName" /></br>
-            Author <input type="text" name="txtAuth" id="txtAuth"  /><br>
-            Description<textarea name="txtDescr" id="txtDescr"></textarea><br>
-            <input type="submit" value="Save"/></br>
-            <input type="reset" value="Cancel"/>
+            <div class="form-group">
+                <label for="txtName">Name</label>
+                <input type="text" name="txtName" id="txtName" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label for="txtAuth">Author</label>
+                <input type="text" name="txtAuth" id="txtAuth" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label for="txtAuth">Description</label>
+                <textarea name="txtDescr" id="txtDescr" class="form-control"></textarea><br>
+            </div>
+            <input type="submit" value="Save" class="btn btn-primary"/>
+            <input type="reset" value="Cancel" class="btn btn-warning"/>
         </form>
-    </body>
-</html>
 @stop
