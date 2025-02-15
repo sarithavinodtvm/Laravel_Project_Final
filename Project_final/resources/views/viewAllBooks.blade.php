0dab881211d73505a0061fffaf26de59438c807a
Saritha DR <sarithavinodtvm@gmail.com>
2025-02-16 03:52:28 +0530

    Modal to edit book details

--- Project_final/resources/views/viewAllBooks.blade.php
+++ Project_final/resources/views/viewAllBooks.blade.php
@@ -22,15 +22,15 @@
             <td>{{$book->bookAuthor}}</td>
             <td>{{$book->bookDescription}}</td>
             <td>
-                <a href="loadEditBookPage" class="btn btn-success"> Edit </a>
-                <a class="btn btn-danger" onClick="$('#demoModal').modal('show')"> Delete </a>
+                <a class="btn btn-success" onClick="$('#editModal').modal('show')" > Edit </a>
+                <a class="btn btn-danger" onClick="$('#deleteModal').modal('show')"> Delete </a>
             </td>
         </tr>
     @endforeach
 </table>
 
 {{--Modal for delete confirmation--}}
-<div class="modal fade" id="demoModal" tabindex="-1" role="dialog" aria-labelledby="demoModalLabel" aria-hidden="true">
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="demoModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
@@ -49,6 +49,42 @@
 		</div>
 	</div>
 </div>
-{{--end of Modal for delete confirmation--}}
+{{--end of Modal}}
+
+{{--Modal for populating book details for updation--}}
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="demoModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="demoModalLabel">Edit Book Details</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+			</div>
+            <form name="frmReg" method="post" action="saveBookDetails" onSubmit="return validateBookDetails()" >
+			<div class="modal-body">
+                @csrf
+                <div class="form-group">
+                    <label for="txtName">Name</label>
+                    <input type="text" name="txtName" id="txtName" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="txtAuth">Author</label>
+                    <input type="text" name="txtAuth" id="txtAuth" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="txtAuth">Description</label>
+                    <textarea name="txtDescr" id="txtDescr" class="form-control"></textarea><br>
+                </div>
+			</div>
+			<div class="modal-footer">
+                <input type="submit" value="Save" class="btn btn-primary"/>
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+			</div>
+            </form>
+		</div>
+	</div>
+</div>
+{{--end of Modal--}}
 
 @stop
